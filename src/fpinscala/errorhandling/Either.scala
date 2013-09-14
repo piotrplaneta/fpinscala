319f01f7f7a229dd35f794dc136a7e93ea23ea05
piotrplaneta <piotr.planeta@gmail.com>
2013-09-14 21:32:53 +0200

    Add Either type

---
+++ src/fpinscala/errorhandling/Either.scala
@@ -0,0 +1,59 @@
+package fpinscala.errorhandling
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: piotrplaneta
+ * Date: 9/14/13
+ * Time: 7:57 PM
+ */
+sealed trait Either[+E,+A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(x) => Right(f(x))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(x) => f(x)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(x) => this
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    for {
+      x <- this
+      y <- b
+    } yield(f(x, y))
+  }
+}
+case class Left[+E](get: E) extends Either[E,Nothing]
+case class Right[+A](get: A) extends Either[Nothing,A]
+
+object Either {
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] = {
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+  }
+
+  def safeDiv(x: Double, y: Double): Either[Exception, Double] = {
+    try {
+      Right(x / y)
+    } catch {
+      case e: Exception => Left(e)
+    }
+  }
+
+  def traverse[E, A, B](a: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    a.foldRight(Right(Nil): Either[E, List[B]])((x, listInEither) =>
+      f(x).map2(listInEither)( _ :: _)
+    )
+  }
+
+  def sequence[E, A](a: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(a)(x => x)
+}
